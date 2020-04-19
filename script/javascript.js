d0fc8eae6406af35ab398a33d38e846598a5d7e3
Douglas Rosa <douglasrosa0110@gmail.com>
2020-04-18 21:35:26 -0300

    Finishing the task e testing

--- script/javascript.js
+++ script/javascript.js
@@ -1,4 +1,3 @@
-/** Initialize MDC Web components. */
 const buttons = document.querySelectorAll('.mdc-button');
 for (const button of buttons) {
   mdc.ripple.MDCRipple.attachTo(button);
@@ -9,28 +8,26 @@ for (const textField of textFields) {
   mdc.textField.MDCTextField.attachTo(textField);
 }
 
-/** Custom javascript code. */
 const calculateMessageEl = document.querySelector('.calculate-message');
 const calculateButton = document.querySelector('.calculate-button');
 
 calculateButton.addEventListener('click', () => {
-  
-  const y = document.querySelector('.calculus-y-input').value;
-  const inteval = document.querySelector('.calculus-interval-input').value;
-  const divisions = document.querySelector('.calculus-divisions-input').value;
+  y = document.querySelector('.calculus-y-input').value;
+  beginX = parseInt(document.querySelector('.calculus-begin-x-input').value);
+  endX = parseInt(document.querySelector('.calculus-end-x-input').value);
+  divisions = parseInt(document.querySelector('.calculus-divisions-input').value);
+  total = 0;
 
-/*
-  if (firstNameInput || lastNameInput) {
-    name = firstNameInput + ' ' + lastNameInput;
-  } else {
-    name = '0.0';
+  absInterval = Math.abs(beginX - endX)
+  baseOfRectangle = absInterval/divisions
+
+  for (var i = 1; i <= divisions; i++) {
+  	xPoint = beginX + baseOfRectangle*i
+  	yWithVarsReplaced = y.replace(/x/, xPoint)
+  	yAplicable = math.compile(yWithVarsReplaced).evaluate()
+  	total += baseOfRectangle * yAplicable
   }
-*/
-	var total = (2-1)*divisions;
 
-	//TODO
-
-	calculateMessageEl.textContent = `result: ${total}`;
-
+  calculateMessageEl.textContent = `result: ${total}`;
 });
 
